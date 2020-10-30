e22ac58a2c8334e54cbe5952c6f027fc5005799d
Mateo Contenla <mcontenlaf@gmail.com>
2020-10-29 21:07:04 -0300

    Arreglo indentación 2: La venganza de los tabs

--- main.rb
+++ main.rb
@@ -6,43 +6,42 @@ require_relative 'src/common/waid_exception'
 require 'optparse'
 
 def main
-
-    @options = {}
-    optparser = OptionParser.new do |op|
-        op.banner += ' filename'
-        op.on("-t", "--show-tokens", "Print the tokens produced by the scanner") do
-            @options[:show_tokens] = true
-        end
-
-        op.on("-a", "--show-ast", "Print the AST produced by the parser") do
-          @options[:show_ast] = true
-        end
+  @options = {}
+  optparser = OptionParser.new do |op|
+    op.banner += ' filename'
+    op.on("-t", "--show-tokens", "Print the tokens produced by the scanner") do
+      @options[:show_tokens] = true
     end
 
-    optparser.parse!
-    # NO SÉ SI ESTA ES LA MANERA CORRECTA DE HACERLO EN RUBY PERDÓN
-    if ARGV.empty?
-        puts optparser
-        exit(-1)
+    op.on("-a", "--show-ast", "Print the AST produced by the parser") do
+      @options[:show_ast] = true
     end
-    file = ARGV.pop
+  end
 
-    begin
-        builder = Builder.new
+  optparser.parse!
+  # NO SÉ SI ESTA ES LA MANERA CORRECTA DE HACERLO EN RUBY PERDÓN
+  if ARGV.empty?
+    puts optparser
+    exit(-1)
+  end
+  file = ARGV.pop
 
-        if @options[:show_tokens]
-            builder.set_show_tokens
-        end
+  begin
+    builder = Builder.new
 
-        if @options[:show_ast]
-          builder.set_show_ast
-        end
-        
-        builder.source_path = file
-        builder.run
-    rescue WaidError => error
-        print error.msg
+    if @options[:show_tokens]
+      builder.set_show_tokens
     end
+
+    if @options[:show_ast]
+      builder.set_show_ast
+    end
+
+    builder.source_path = file
+    builder.run
+  rescue WaidError => error
+    print error.msg
+  end
 end
 
 main
--- src/builder.rb
+++ src/builder.rb
@@ -7,46 +7,46 @@ require_relative 'parser/parser'
 class Builder
   attr_writer :source_path
   def initialize()
-	@show_tokens = false
-	@show_ast = false
-	@source_path = String.new
+    @show_tokens = false
+    @show_ast = false
+    @source_path = String.new
   end
 
   def set_show_tokens
-	@show_tokens = true
+    @show_tokens = true
   end
 
   def set_show_ast
-	@show_ast = true
+    @show_ast = true
   end
 
   def run
-	source_file = WaidFile.new(@source_path)
+    source_file = WaidFile.new(@source_path)
 
-	error_collector = ErrorCollector.new(source_file)
+    error_collector = ErrorCollector.new(source_file)
 
-	tokenizer = Tokenizer.new(source_file, error_collector)
-	tokenizer.tokenize!
+    tokenizer = Tokenizer.new(source_file, error_collector)
+    tokenizer.tokenize!
 
-	if error_collector.hasErrors
-	  error_collector.showErrors
-	end
+    if error_collector.hasErrors
+      error_collector.showErrors
+    end
 
-	if @show_tokens
-	  tokenizer.tokens.each do |tok|
-		puts "#{tok.get_line_number}| #{tok.to_s} #{tok.value}"
-	  end
-	end
+    if @show_tokens
+      tokenizer.tokens.each do |tok|
+        puts "#{tok.get_line_number}| #{tok.to_s} #{tok.value}"
+      end
+    end
 
-	parser = Parser.new(tokenizer.tokens, error_collector)
-	parser.parse!
+    parser = Parser.new(tokenizer.tokens, error_collector)
+    parser.parse!
 
-	if @show_ast
-	  parser.ast.to_string
-	end
+    if @show_ast
+      parser.ast.to_string
+    end
 
-	if error_collector.hasErrors
-	  error_collector.showErrors
-	end
+    if error_collector.hasErrors
+      error_collector.showErrors
+    end
   end
 end
